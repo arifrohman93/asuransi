cfcb37b8c577cd557bc38dd70f3620e6c463a4a1
bagas.yeremi <bagas.yeremi@external.primecash.co.id>
2024-12-17 17:29:33 +0700

    update asuransi application

---
+++ src/main/java/com/example/asuransi/model/Role.java
@@ -0,0 +1,24 @@
+@Entity
+@Table(name = "m_role")
+public class Role {
+    @Id
+    private String roleName;
+
+    private String keterangan;
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getKeterangan() {
+        return keterangan;
+    }
+
+    public void setKeterangan(String keterangan) {
+        this.keterangan = keterangan;
+    }
+}
---
+++ src/main/java/com/example/asuransi/security/JwtTokenProvider.java
@@ -0,0 +1,36 @@
+@Component
+public class JwtTokenProvider {
+
+    private final String jwtSecret = "mysupersecretkey";
+    private final long jwtExpirationInMs = 3600000;
+
+    public String generateToken(Authentication authentication) {
+        String username = authentication.getName();
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + jwtExpirationInMs);
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public String getUsernameFromJWT(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        } catch (Exception ex) {
+            throw new RuntimeException("Invalid JWT token");
+        }
+    }
+}
